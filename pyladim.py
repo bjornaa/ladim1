836d9786d4eb3be2cca741fe3eba788933b5bfde
Bjørn Ådlandsvik <bjorn@imr.no>
2012-10-04 10:59:22 +0200

    Working prototype with particle release

--- pyladim.py
+++ pyladim.py
@@ -5,6 +5,7 @@ import numpy as np    # Midlertidig, unødvendig når skikkelig initiering
 from netCDF4 import num2date
 from trackpart import Euler_Forward 
 from input import ROMS_input
+from release import ParticleReleaser
 from setup import readsup, writesup
 from output import OutPut
 
@@ -43,10 +44,18 @@ tunits = inp.nc.variables['ocean_time'].units
 # Init some particles
 # --------------------
 
-Npart = 10
-particle_id = np.arange(1, Npart+1, dtype='int')
-X = np.linspace(96.0, 122.0, num=Npart)
-Y = 100.0 + np.zeros_like(X)
+# Lage et objekt som holder partiklene ??
+
+pid   = np.array([], dtype='int32')
+X     = np.array([], dtype='float32')
+Y     = np.array([], dtype='float32')
+Z     = np.array([], dtype='float32')
+start = np.array([], dtype='int32')
+
+#particle_release_file = 'particles.in'
+particle_release_file = 'line.in'
+partini = ParticleReleaser(particle_release_file, setup['dt'])
+
 
 # -----------
 # Init output
@@ -64,6 +73,15 @@ for i in range(nsteps+1):
     inp.update(i)
     #print "F = ", inp.F, num2date(inp.F, tunits)
 
+    # Read particles ?
+    if i == partini.release_step:
+        partini.read_particles()
+        pid = np.concatenate((pid, partini.pid))
+        X = np.concatenate((X, partini.X))
+        Y = np.concatenate((Y, partini.Y))
+        Z = np.concatenate((Z, partini.Z))
+        start = np.concatenate((start, partini.start))
+        
     # Save to file 
     if i % setup['output_period'] == 0:
         out.write(i, X, Y)
