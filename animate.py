9ecbe402e9d333e4b13128db0f1f3163a7cf70cb
Bjørn Ådlandsvik <bjorn@imr.no>
2012-10-04 11:37:01 +0200

    added some tools including animate.py

---
+++ animate.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from netCDF4 import Dataset, num2date
+import roppy
+from roppy.mpl_util import landmask
+
+i0, j0 = 70,   80
+i1, j1 = 140, 130
+
+
+f0 = Dataset('data/ocean_avg_0014.nc')
+g = roppy.SGrid(f0, subgrid=(i0,i1,j0,j1))
+
+#f  = Dataset('/tmp/pyladim_out.nc')
+f  = Dataset('pyladim_out.nc')
+
+Ntimes = len(f.dimensions['Time'])
+
+#Xc, Yc = np.loadtxt("./data/ns8_coast.dat", unpack=True)
+#Xc = Xc - i0
+#Yc = Yc - j0
+
+#H = f0.variables['h'][:,:]
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+#ax.set_xlim(70,140)
+#ax.set_ylim(80,130)
+#ax.axis('image')
+
+def read(t):
+    p0 = f.variables['pStart'][t]
+    Npart = f.variables['pCount'][t]
+    tid = f.variables['time'][t]
+    tunit = f.variables['time'].units
+    #print p0, Npart
+    timestr = num2date(tid, tunit)
+    X = f.variables['X'][p0:p0+Npart]
+    Y = f.variables['Y'][p0:p0+Npart]
+    # Not handling subgrids properly??
+    X = X - i0
+    Y = Y - j0
+    return X, Y, timestr
+
+
+def animate():
+    for t in range(1, Ntimes):
+        X, Y, tstring = read(t)
+        h[0].set_xdata(X)
+        h[0].set_ydata(Y)
+        ax.set_title(tstring)
+        fig.canvas.draw()
+
+X, Y, tstring = read(0)
+
+cmap = plt.get_cmap('Blues')
+
+ax.contourf(g.h, cmap=cmap)
+#ax.contour(g.h, levels = [10.0], colors='black', linewidths=2.5)
+roppy.mpl_util.landmask(g.mask_rho, (0.6, 0.8, 0.0))
+
+#plt.fill(Xc, Yc, edgecolor=(0.6, 0.8, 0.0))
+
+
+h = ax.plot(X, Y, '.', color='red')
+ax.set_title(tstring)
+
+
+fig.canvas.manager.window.after(100, animate)
+plt.show()
+
--- plot.py
+++ plot.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 from netCDF4 import Dataset, num2date
 
-f = Dataset('pyladim_out.nc')
+f0 = Dataset('data/ocean_avg_0014.nc')
+f  = Dataset('pyladim_out.nc')
 
 t = 6
-
 
 p0 = f.variables['pStart'][t]
 Npart = f.variables['pCount'][t]
@@ -17,8 +17,16 @@ timestr = num2date(tid, tunit)
 X = f.variables['X'][p0:p0+Npart]
 Y = f.variables['Y'][p0:p0+Npart]
 
-plt.plot(X, Y)
+H = f0.variables['h'][:,:]
+
+plt.plot(X, Y, 'o', color='blue')
 plt.title(timestr)
+
+
+plt.contour(H, levels = [50.0, 100.0, 200.0, 500.0], colors='black')
+plt.contour(H, levels = [10.0], colors='black', linewidths=2.5)
+
+plt.axis('image')
 
 plt.show()
 
