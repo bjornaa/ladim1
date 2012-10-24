b1bc68e29fcc354fbe746cea79b68d9e1f42dce3
Bjørn Ådlandsvik <bjorn@imr.no>
2012-10-24 22:18:54 +0200

    working track.py and improved particlefile.py

--- postpro/particlefile.py
+++ postpro/particlefile.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
 from netCDF4 import Dataset, num2date
 #import roppy
@@ -30,9 +32,46 @@ class ParticleFile(object):
         # put in some error control
         return f.variables[vname][p0:p0+Npart]
 
-    def read_track(self, pid):
+    def read_track(self, pid0):
         """Get particle positions along a track"""
-        pass
+
+        # Tips, returner array med indeks-verdier
+        # kanskje bare indeks verdier i hovedarray
+        # => kan lese vilkårlige verdier
+        # også ha en variant, 
+
+        nc = self.nc
+
+
+        X, Y = [], []
+        first_time = None  
+        last_time  = None  
+
+        # After loop
+        # particle is alive for n in [first_time:last_time]
+        # or to the end if last_time == 0
+
+        for n in xrange(self.nFrames):
+            pstart = nc.variables['pstart'][n]
+            pcount = nc.variables['pcount'][n]
+            pid = nc.variables['pid'][pstart:pstart+pcount][:]
+
+            if pid[-1] < pid0: # particle not released yet
+                cycle
+
+            if first_time != None:
+                first_time = n
+
+            #index = sum(pid < pid0) # eller lignende
+            index = pid.searchsorted(pid0)
+            if pid[index] < pid0: # pid0 is missing
+                last_time = n     # 
+                break             # No need for more cycles
+    
+            X.append(nc.variables['X'][pstart + index])
+            Y.append(nc.variables['Y'][pstart + index])
+
+        return X, Y, first_time, last_time
 
     def close(self):
         self.nc.close()
--- postpro/track.py
+++ postpro/track.py
@@ -2,19 +2,32 @@
 
 # Make a track of one particle
 
-
+import matplotlib.pyplot as plt
 from netCDF4 import Dataset
+import roppy.mpl_util
 
-
-pid0 = 1222 
+pid0 = 8222 
 
 particle_file = "../output/pyladim_out.nc"
+roms_file     = '../input/ocean_avg_0014.nc'
+
+# Subgrid definition
+i0, j0 = 70,   80
+i1, j1 = 150, 133
+
+# ----------------
+
+# ROMS grid, plot domain
+
+f0 = Dataset(roms_file)
+g = roppy.SGrid(f0, subgrid=(i0,i1,j0,j1))
+f0.close()
 
 
 
 f = Dataset(particle_file)
 
-Ntimes = len(f.dimension['time'])
+Ntimes = len(f.dimensions['time'])
 
 X, Y = [], []
 first_time = None  
@@ -23,10 +36,10 @@ last_time  = None
 # particle is alive for n in [first_time:last_time]
 # or to the end if last_time == 0
 
-for n in Ntimes:
+for n in xrange(Ntimes):
     pstart = f.variables['pstart'][n]
     pcount = f.variables['pcount'][n]
-    pid = f.variables[pstart:pstart+pcount][:]
+    pid = f.variables['pid'][pstart:pstart+pcount][:]
 
     if pid[-1] < pid0: # particle not released yet
         cycle
@@ -40,7 +53,20 @@ for n in Ntimes:
         last_time = n     # 
         break             # No need for more cycles
     
-    X.append(f.variables
+    X.append(f.variables['X'][pstart + index] - i0)
+    Y.append(f.variables['Y'][pstart + index] - j0)
+
+# Make plot
+
+cmap = plt.get_cmap('Blues')
+plt.contourf(g.h, cmap=cmap, alpha=0.3)
+roppy.mpl_util.landmask(g.mask_rho, (0.6, 0.8, 0.0))
+
+plt.plot(X[0], Y[0], 'rx')
+plt.plot(X, Y, '-')
+
+plt.axis('image')
+plt.show()
 
     
 
