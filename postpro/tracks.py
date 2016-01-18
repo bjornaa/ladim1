cbcbe002f854769bc9d2c416889363b0b8036003
Bjørn Ådlandsvik <bjorn@imr.no>
2016-01-18 09:19:03 +0100

    Updated postpro/tracks.py

--- postpro/tracks.py
+++ postpro/tracks.py
@@ -12,8 +12,8 @@ from netCDF4 import Dataset
 import roppy.mpl_util
 from particlefile import ParticleFile
 
-#pids = [10,8222]
-pids = np.arange(10,10000,1000)
+# pids = [10, 8222]
+pids = np.arange(10, 10000, 1000)
 
 particle_file = "../output/pyladim_out.nc"
 roms_file     = '../input/ocean_avg_0014.nc'
@@ -27,31 +27,22 @@ i1, j1 = 150, 133
 # ROMS grid, plot domain
 
 f0 = Dataset(roms_file)
-g = roppy.SGrid(f0, subgrid=(i0,i1,j0,j1))
-f0.close()
-
-
+g = roppy.SGrid(f0, subgrid=(i0, i1, j0, j1))
 
 pf = ParticleFile(particle_file)
 
 X, Y = pf.read_tracks(pids)
-X = np.array(X) - i0
-Y = np.array(Y) - j0
-
-#print X
+X = np.array(X)
+Y = np.array(Y)
 
 # Make plot
 
 cmap = plt.get_cmap('Blues')
-plt.contourf(g.h, cmap=cmap, alpha=0.3)
-roppy.mpl_util.landmask(g.mask_rho, (0.6, 0.8, 0.0))
+plt.contourf(g.X, g.Y, g.h, cmap=cmap, alpha=0.3)
+roppy.mpl_util.landmask(g, (0.6, 0.8, 0.0))
 
 plt.plot(X[0], Y[0], 'ko')
 plt.plot(X, Y, '-')
 
 plt.axis('image')
 plt.show()
-
-    
-
-
