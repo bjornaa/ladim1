be2e0f83baf1849cafe16c863f2fa846927d0598
Bjørn Ådlandsvik <bjorn@imr.no>
2017-01-26 15:27:05 +0100

    Added logging and time check to release

--- ladim/release.py
+++ ladim/release.py
@@ -1,5 +1,34 @@
 # Particle release class
 
+# -------------------
+# release.py
+# part of LADIM
+# --------------------
+
+# ----------------------------------
+# Bjørn Ådlandsvik <bjorn@imr.no>
+# Institute of Marine Research
+# Bergen, Norway
+# ----------------------------------
+
+# Timing
+# Allow release both before or after stop/start time
+# A warning is issued and the particles ignored.
+#
+# Usage: A large release file, but running bits and pieces
+# with restart
+#
+# particle release at start_time are considered
+# particle release at stop_time is not considered?
+#
+# Has to think more about restart:
+# start of next = last time on output file
+# restart: do not write initial
+
+# First release after start_time: issue warning
+# Warning must be: ladim running with no particles
+
+# For restart, no problem: t
 
 # Sequence:
 # date clock mult X Y Z [particle_variables] [state_variables]
@@ -13,21 +42,28 @@
 # date clock mult X Y Z farm_id age super
 # 2016-03-11 6 3 366 464 5 10147 0 1000
 
+import logging
 import numpy as np
 import pandas as pd
 
 # ------------------------
 
-# Skal ha public:
-# release_steps, slik at en kan sjekke om release_step
-# i så fall kalle generator prel.release()
-# som returnerer dictionary med release_variablene
-# evt, legger de selv til i state
+logger = logging.getLogger(__name__)
+ch = logging.StreamHandler()
+formatter = logging.Formatter('%(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 
 class ParticleReleaser():
+    """Particle Release Class"""
 
-    def __init__(self, config):
+    def __init__(self, config, loglevel=logging.INFO):
+
+        # print("loglevel = ", loglevel)
+        # raise SystemExit
+        logger.setLevel(loglevel)
+        # logger.setLevel(logging.INFO)
 
         # Store the data in a pandas dataframe
         self._df = pd.read_csv(
@@ -36,6 +72,23 @@ class ParticleReleaser():
             dtype=config.release_dtype,
             parse_dates=['release_time'],
             delim_whitespace=True)
+
+        # Time control
+        if self._df.release_time[0] < config.start_time:
+            logger.warning('Ignoring particles release before start')
+            self._df = self._df[self._df.release_time >= config.start_time]
+        if self._df.release_time.iloc[-1] >= config.stop_time:
+            logger.warning('Ignoring particles release after stop')
+            self._df = self._df[self._df.release_time < config.stop_time]
+        # May be modified with restart
+        if self._df.release_time[0] > config.start_time:
+            logger.warning('No particles at start time')
+
+        logger.info('First particle release at {}'.
+                    format(str(self._df.release_time[0])))
+        logger.info('Last particle release at  {}'.
+                    format(self._df.release_time.iloc[-1]))
+        logger.info('Number of particle releases = {}'.format(len(self._df)))
 
         # Relative time
         rel_time = self._df['release_time'] - config.start_time
@@ -49,6 +102,8 @@ class ParticleReleaser():
         # The total number of particles released during the simulation
         # config['total_particle_count'] = self._df['mult'].sum()
         config.total_particle_count = self._df['mult'].sum()
+        logger.info('Total number of particles in simulation: {}'.
+                    format(config.total_particle_count))
 
         self._npids = 0    # Number of particles released
         self._release_index = 0
@@ -75,8 +130,8 @@ class ParticleReleaser():
 
     def __next__(self):
         timestep = self.release_steps[self._release_index]
-        print('release: timestep, time = ', timestep,
-              self.release_times[self._release_index])
+        logger.info('release: timestep, time = {}, {}'.
+                    format(timestep, self.release_times[self._release_index]))
         # print(type(timestep))
         self._release_index += 1
 
