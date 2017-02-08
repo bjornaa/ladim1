bbfa0f2f34c56d19fd127f075cf4514848247a96
Bjørn Ådlandsvik <bjorn@imr.no>
2017-02-08 16:07:32 +0100

    Vertical interpolation working

--- test/test_sdepth.py
+++ test/test_sdepth.py
@@ -170,124 +170,52 @@ class test_Vstretching4(unittest.TestCase):
 class test_Z2S(unittest.TestCase):
 
     def test_correct(self):
+        # Set-up
         N = 30
         H = 100 + np.zeros((1, 1))
         Hc = 10.0
         # Make a random increasing sequence of N values between -1 to 0
-        C = np.random.uniform(-1, 0, N+1)
+        # with end points -1 and 0
+        C = np.zeros(N+1)
+        C[0] = -1
+        C[1:-1] = np.random.uniform(-1, 0, N-1)
         C.sort()
-        Z = 50.0
         z_w = sdepth(H, Hc, C, stagger='w')
-        assert(z_w.shape == (31, 1, 1))
+
+        # at surface
+        Z = 0.0
         K, A = Z2S(z_w, 0.5, 0.5, Z)
-        # assert(K == 3)
-        assert(z_w[K] <= -Z)
-        assert(-Z <= z_w[K+1])
+        assert(z_w[K] <= -Z <= z_w[K+1])
         assert(0 <= A <= 1)
-        assert(A == 3.14)
-        assert(A*z_w[K] + (1-A)*z_w[K+1] == -Z)
+        assert(abs(A*z_w[K] + (1-A)*z_w[K+1] + Z) < 1.e-5)
 
+        # near surface
+        Z = 0.1
+        K, A = Z2S(z_w, 0.5, 0.5, Z)
+        assert(z_w[K] <= -Z <= z_w[K+1])
+        assert(0 <= A <= 1)
+        assert(abs(A*z_w[K] + (1-A)*z_w[K+1] + Z) < 1.e-5)
 
-# ----------------------------------------------------
+        # mid water
+        Z = 50
+        K, A = Z2S(z_w, 0.5, 0.5, Z)
+        assert(z_w[K] <= -Z <= z_w[K+1])
+        assert(0 <= A <= 1)
+        assert(abs(A*z_w[K] + (1-A)*z_w[K+1] + Z) < 1.e-5)
 
-    # def test_linear_interpolation(self):
-    #     """The interpolation is linear"""
-    #     z_r = np.linspace(-95, -5, 10)
-    #     F = np.random.random(z_r.shape)
-    #     a, b = 0.3, 0.7 # sum = 1
-    #     k = 4
-    #     z = a*z_r[k-1] + b*z_r[k]
-    #     v = a*F[k-1] + b*F[k]
-    #     self.assertAlmostEqual(zslice(F, z_r, z), v)
-    #
-    # def test_extrapolate(self):
-    #     """Extrapolation should extend highest and lowest values"""
-    #     z_r = np.linspace(-95, -5, 10)
-    #     F = np.random.random(z_r.shape)
-    #     z = -2.0     # z > z_r[-1]
-    #     self.assertEqual(zslice(F, z_r, z), F[-1]) # high value
-    #     z = -122.3   # z < z_r[0]
-    #     self.assertEqual(zslice(F, z_r, z), F[0])  # low value
-    #
-    # def test_interp(self):
-    #     """zslice of 1D field is np.interp"""
-    #     z_r = np.linspace(-95, -5, 10)
-    #     F = np.random.random(z_r.shape)
-    #     z = -52.0
-    #     self.assertAlmostEqual(zslice(F, z_r, z), np.interp(z, z_r, F))
-    #
-    # def test_non_equidistant(self):
-    #     """zslice should work with non-equidistand z_r"""
-    #     # Make a random increasing sequence z_r between -100 and 0
-    #     z_r = -100 * np.random.rand(10)
-    #     z_r.sort()
-    #     F = np.random.rand(10)
-    #     z = -52.0
-    #     self.assertAlmostEqual(zslice(F, z_r, z), np.interp(z, z_r, F))
-    #
-    #
-    # def test_shapes_OK(self):
-    #     """Correct input shapes gives correct output shape"""
-    #
-    #     # Make a z_r array of shape (N, Mp, Lp)
-    #     # in this case (10,3,2)
-    #     K = np.linspace(-95, -5, 10)
-    #     z_r = np.transpose([[K, 2*K, K], [K, 0.7*K, 1.2*K]])
-    #     # F has same shape
-    #     F = np.random.random(z_r.shape)
-    #     # Scalar z
-    #     z = -52.0
-    #     self.assertEqual(zslice(F,z_r,z).shape, z_r.shape[1:])
-    #     # Horizontal z with shape z_r.shape[1:] = (Mp, Lp)
-    #     z =  np.array([-10.2, -14., -28, -2.4, -88.7, -122])
-    #     z.shape = (3,2)  # z has correct shape
-    #     self.assertEqual(zslice(F,z_r,z).shape, z_r.shape[1:])
-    #
-    # def test_Fshape_wrong(self):
-    #     """Raise exception if F.shape != z_r.shape"""
-    #
-    #     # Make a z_r array of shape (N, Mp, Lp)
-    #     # in this case (10,3,2)
-    #     K = np.linspace(-95, -5, 10)
-    #     z_r = np.transpose([[K, 2*K, K], [K, 0.7*K, 1.2*K]])
-    #     # F has same shape
-    #     F = np.random.random(z_r.shape)
-    #     F.shape  = (10,2,3)  # Give F wrong shape
-    #     z = -52.0
-    #     self.assertRaises(ValueError, zslice, F, z_r, z)
-    #
-    # def test_zshape_wrong(self):
-    #     """Raise exception if z.shape is wrong"""
-    #
-    #     # Make a z_r array of shape (N, Mp, Lp)
-    #     # in this case (10,3,2)
-    #     K = np.linspace(-95, -5, 10)
-    #     z_r = np.transpose([[K, 2*K, K], [K, 0.7*K, 1.2*K]])
-    #     # F has same shape
-    #     F = np.random.random(z_r.shape)
-    #     z =  np.array([-10.2, -14., -28, -2.4, -88.7, -122])
-    #     z.shape = (2,3)   # z is 2D, correct size, wrong shape
-    #     self.assertRaises(ValueError, zslice, F, z_r, z)
-    #
-    # def test_array_values(self):
-    #     """Test that interpolation gives correct results with array input"""
-    #     # Make a z_r array of shape (N, Mp, Lp)
-    #     # in this case (10,3,2)
-    #     K = np.linspace(-95, -5, 10)
-    #     z_r = np.transpose([[K, 2*K, K], [K, 0.7*K, 1.2*K]])
-    #     # F has same shape
-    #     F = np.random.random(z_r.shape)
-    #     j, i = 2, 1
-    #     z = -52.0                # z is scalar
-    #     Fz = zslice(F, z_r, z)
-    #     self.assertAlmostEqual(Fz[j,i], np.interp(z, z_r[:,j,i], F[:,j,i]))
-    #
-    #     z =  np.array([-10.2, -14., -28, -2.4, -88.7, -122])
-    #     z.shape = (3,2)  # z has correct shape
-    #     Fz = zslice(F, z_r, z)
-    #     self.assertAlmostEqual(Fz[j,i],
-    #                            np.interp(z[j,i], z_r[:,j,i], F[:,j,i]))
+        # near bottom
+        Z = H[0, 0] - 0.1
+        K, A = Z2S(z_w, 0.5, 0.5, Z)
+        assert(z_w[K] <= -Z <= z_w[K+1])
+        assert(0 <= A <= 1)
+        assert(abs(A*z_w[K] + (1-A)*z_w[K+1] + Z) < 1.e-5)
 
+        # at bottom
+        Z = H[0, 0]
+        K, A = Z2S(z_w, 0.5, 0.5, Z)
+        assert(z_w[K] <= -Z <= z_w[K+1])
+        assert(0 <= A <= 1)
+        assert(abs(A*z_w[K] + (1-A)*z_w[K+1] + Z) < 1.e-5)
 
 # --------------------------------------
 
