4a80d3b1011b9367dedaece7ed32248fd80391b9
Bjørn Ådlandsvik <bjorn@imr.no>
2020-03-03 15:56:00 +0100

    Added holomap notebook to examples/line

--- examples/line/plot_holoviews.py
+++ examples/line/plot_holoviews.py
@@ -4,16 +4,18 @@ from pathlib import Path
 import numpy as np
 import xarray as xr
 import holoviews as hv
+import cmocean
 from postladim import ParticleFile
 
 hv.extension("bokeh")
 
 # --- Settings ---
-tstep = 40   # Time step to show
+
+tstep = 40  # Time step to show
 # Output file (and type)
 output_file = "line_hv.png"
-#output_file = "line_hv.html"
-scale = 5   # Figure size factor
+# output_file = "line_hv.html"
+scale = 5  # Figure size factor
 
 # --- Data files ---
 
@@ -21,17 +23,17 @@ ladim_dir = Path("../../")
 grid_file = ladim_dir / "examples/data/ocean_avg_0014.nc"
 particle_file = ladim_dir / "examples/line/line.nc"
 
-# --- Read particle data ---
+# --- Particle data ---
 
-pf = ParticleFile(particle_file)
-X, Y = pf.position(tstep)
+with ParticleFile(particle_file) as pf:
+    XY = pf.position(tstep)
 
 # --- Background bathymetry data ---
 
 # Read bathymetry and land mask
-with xr.open_dataset(grid_file) as A:
-    H = A.h
-    M = A.mask_rho
+with xr.open_dataset(grid_file) as ds:
+    H = ds.h  # Bottom topography
+    M = ds.mask_rho  # Land mask
 jmax, imax = M.shape
 H = H.where(M > 0)  # Mask out land
 M = M.where(M < 1)  # Mask out sea
@@ -42,19 +44,20 @@ M = M.where(M < 1)  # Mask out sea
 land = hv.Image(data=M, kdims=["xi_rho", "eta_rho"], group="Land")
 
 # Bathymetry image
-topo = hv.Image(data=-np.log10(H), kdims=["xi_rho", "eta_rho"], group="Topo")
+# Take logarithm to show details in the shallow North Sea
+topo = hv.Image(data=np.log10(H), kdims=["xi_rho", "eta_rho"], group="Topo")
 
 # Particle distribution
-spread = hv.Scatter(data=(X, Y))
+spread = hv.Points(data=XY)
 
 # Overlay
 h = topo * land * spread
 
 # --- Plot options ---
 
-h.opts(frame_width=scale * imax, frame_height=scale * jmax)
-h.opts("Scatter", color="red")
-h.opts("Image.Topo", cmap="blues_r", alpha=0.7)
+h.opts(frame_width=int(scale * imax), frame_height=int(scale * jmax))
+h.opts("Points", size=2, color="red")
+h.opts("Image.Topo", cmap="blues", alpha=0.7)
 h.opts("Image.Land", cmap=["#80B040"])
 
 # --- Save output ---
