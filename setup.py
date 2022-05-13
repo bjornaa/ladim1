ae7e7bcee9efde474470b32b67c049f802441339
Ådlandsvik Bjørn <bjorn@imr.no>
2022-05-13 13:02:39 +0200

    Decoupled postladim (but it is still here)

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(
     description="Lagrangian Advection and Diffusion Model",
     author="Bjørn Ådlandsvik",
     author_email="bjorn@imr.no",
-    packages=["ladim", "postladim", "ladim.ibms", "ladim.gridforce"],
+    #packages=["ladim", "postladim", "ladim.ibms", "ladim.gridforce"],
+    packages=["ladim", "ladim.ibms", "ladim.gridforce"],
     scripts=["scripts/ladim"],
     requires=["numpy", "yaml", "netCDF4", "pandas"],
 )
